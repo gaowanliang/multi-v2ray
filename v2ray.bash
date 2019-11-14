c66cb921484150535e9544f5ea945336a2366918
gaowanliang <1175373504@qq.com>
2019-11-14 19:50:28 +0800

    update

--- v2ray.bash
+++ v2ray.bash
@@ -1,20 +1,23 @@
 # bash completion for v2ray                             -*- shell-script -*-
 function _auto_tab() {
-	local options_array=("start" "stop" "restart" "status" "update" "add" "del" "info" "port" "tls" "tfo" "stream" "stats" "clean" "log" "new" "-h" "-v")
+	local options_array=("start" "stop" "restart" "status" "update" "add" "del" "info" "port" "tls" "tfo" "stream" "stats" "clean" "log" "new" "fcu" "ll" "ht" "gb" "delu" "ban" "restore" "-h" "-v")
 	local add_array=("wechat" "utp" "srtp" "dtls" "wireguard" "socks" "mtproto" "ss")
 	local cur pre
 
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	prev="${COMP_WORDS[COMP_CWORD - 1]}"
 
 	case $prev in
-		'v2ray')
-			COMPREPLY=( $(compgen -W "${options_array[*]}" -- $cur) ) ;;
-		'add')
-			COMPREPLY=( $(compgen -W "${add_array[*]}" -- $cur) );;
-		'*')
-			COMPREPLY=( $(compgen -d -f ${cur}) ) ;;
+	'v2ray')
+		COMPREPLY=($(compgen -W "${options_array[*]}" -- $cur))
+		;;
+	'add')
+		COMPREPLY=($(compgen -W "${add_array[*]}" -- $cur))
+		;;
+	'*')
+		COMPREPLY=($(compgen -d -f ${cur}))
+		;;
 	esac
 
 	return 0
